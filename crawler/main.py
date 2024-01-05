c28a5c8dad90bb273aea639d5961ba66c1364260
u120022 <u120022@st.pu-toyama.ac.jp>
2024-01-05 16:08:28 +0900

    initial commit

---
+++ crawler/main.py
@@ -0,0 +1,24 @@
+import asyncio
+
+import lib
+
+
+async def main():
+    # points = await lib.async_get_pano_recursive(36.7447799, 137.002534, 2)
+    # print("points: {}".format(len(points)))
+    #
+    # futures = [lib.async_download_pano_img(point.id, 3) for point in points]
+    # for point, img in zip(points, await asyncio.gather(*futures)):
+    #     print("img: {}".format(point.id))
+    #     img.save("img/{}.png".format(point.id))
+
+    rdcl = await lib.async_get_rdcl((137.002534, 36.7447799), (137.012534, 36.7547799))
+    rdcl.to_file("rdcl.geojson", driver="GeoJSON")
+
+    fgd = await lib.async_get_fgd((137.002534, 36.7447799), (137.012534, 36.7547799))
+    fgd.to_file("fgd.geojson", driver="GeoJSON")
+
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
