e5aff9f775bbd266013839cb75fae8c54f091d44
u120022 <u120022@st.pu-toyama.ac.jp>
2024-01-10 09:35:56 +0900

    fmt

--- crawler/rdcl_cross.sql
+++ crawler/rdcl_cross.sql
@@ -1,6 +1,5 @@
--- create target intersection points
-DROP TABLE IF EXISTS rdcl_itp;
-CREATE TABLE rdcl_itp AS (
+DROP TABLE IF EXISTS rdcl_crosspoint;
+CREATE TABLE rdcl_crosspoint AS (
     SELECT
         geom
     FROM (
@@ -18,9 +17,12 @@ CREATE TABLE rdcl_itp AS (
         count(*) = 3 OR count(*) = 4
 );
 
--- culling by target intersection points
-DROP TABLE IF EXISTS rdcl_itl;
-CREATE TABLE rdcl_itl AS (
+-- create index for spacial joining #1
+DROP INDEX IF EXISTS rdcl_crosspoint_idx;
+CREATE INDEX IF NOT EXISTS rdcl_crosspoint_idx ON rdcl_crosspoint USING GIST(geom);
+
+DROP TABLE IF EXISTS rdcl_crossline;
+CREATE TABLE rdcl_crossline AS (
     SELECT
         ST_LongestLine(t2.geom, t1.geom) AS geom
     FROM (
@@ -29,17 +31,15 @@ CREATE TABLE rdcl_itl AS (
         FROM
             rdcl
     ) AS t1 JOIN
-        rdcl_itp
+        rdcl_crosspoint
     AS t2 ON
         ST_Intersects(t1.geom, t2.geom)
 );
 
--- create index #1
-DROP INDEX IF EXISTS rdcl_itp_idx;
-DROP INDEX IF EXISTS rdcl_itl_idx;
-CREATE INDEX IF NOT EXISTS rdcl_itp_idx ON rdcl_itp USING GIST(geom);
-CREATE INDEX IF NOT EXISTS rdcl_itl_idx ON rdcl_itl USING GIST(geom);
+-- create index for spacial joining #2
+DROP INDEX IF EXISTS rdcl_crossline_idx;
+CREATE INDEX IF NOT EXISTS rdcl_crossline_idx ON rdcl_crossline USING GIST(geom);
 
--- create index #2
+-- create index for spacial joining #3
 DROP INDEX IF EXISTS fgd_idx;
 CREATE INDEX IF NOT EXISTS fgd_idx ON fgd USING GIST(geometry);
